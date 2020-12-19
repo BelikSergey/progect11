301f6bd44be6a49efe11fe9d36f41b85a0645b96
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-12-19 12:00:23 +0200

    весь проэкт в src/src

---
+++ src/src/index.js
@@ -0,0 +1,16 @@
+import './style.scss';
+import './js/main-created';
+import './js/user-registration';
+import './js/menu';
+import './js/login-and-registration';
+import './js/add-modal-show';
+import './js/add-modal-categories';
+// import './js/add-favourites-into-api';
+// import './js/add-favourites-into-page';
+import './js/delete-favourites-from-api&page';
+import './js/add-modal-valid';
+import './js/add-modal-post';
+import './js/advert-Close-Open';
+import './js/advert-logic';
+import './js/search-modal';
+import './js/add-del-favourite';
